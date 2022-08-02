852008b5d43d182309c253a7cfddfdcdeafc3250
GreatZue <greatzue@gmail.com>
2022-08-01 21:07:43 -0600

    commit

--- Script.js
+++ Script.js
@@ -10,19 +10,19 @@ var isSunk = false;
 
 function playGame() {
 while(hits < 3) {
-    guess = prompt("Enter a number 1-7!");
+    guess = window.prompt("Enter a number 1-7!");
     guesses++
 
     if(guess >= shipPart1 && guess <= shipPart3) {
-        alert("Nice job, you hit them!");
+        window.alert("Nice job, you hit them!");
         hits++
     }
     else {
-        alert("Oh no, you missed!");
+        window.alert("Oh no, you missed!");
     }
 }
 if (hits == 3) {
     isSunk = true;
 }
-alert("Congratulations, here is your score: " + hits + "/n" + guesses);
+window.alert("Congratulations, here is your score: " + hits + "/n" + guesses);
 }
